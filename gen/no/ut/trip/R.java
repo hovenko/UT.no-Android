bbf655b753f077751893a94e508b039438e9f95f
Knut-Olav Hoven <knutolav@gmail.com>
2010-01-10 17:25:28 +0100

    Fix: label of button was wrong

--- gen/no/ut/trip/R.java
+++ gen/no/ut/trip/R.java
@@ -15,7 +15,7 @@ public final class R {
         public static final int utno=0x7f020001;
     }
     public static final class id {
-        public static final int btn_trips=0x7f050008;
+        public static final int btn_trips=0x7f050007;
         public static final int image_utno=0x7f050000;
         public static final int layout_content=0x7f050006;
         public static final int layout_facet=0x7f050004;
@@ -23,7 +23,6 @@ public final class R {
         public static final int list_facet=0x7f050005;
         public static final int search_btn=0x7f050003;
         public static final int search_text=0x7f050002;
-        public static final int trips=0x7f050007;
     }
     public static final class layout {
         public static final int listings=0x7f030000;
@@ -31,10 +30,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int btn_trips=0x7f040005;
         public static final int hello=0x7f040000;
         public static final int process_loading_data=0x7f040002;
         public static final int search_hint=0x7f040003;
         public static final int tab_locations=0x7f040004;
-        public static final int trips=0x7f040005;
     }
 }
